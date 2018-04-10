44fc2f9375b45d85abc70f6f0c1dba55d0120dda
YuliaShylo <shyloyu@gmail.com>
2018-04-10 18:15:30 +0300

    Исправлена высота колонок и расположение

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,3 @@
-
-// Константы
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_HEIGHT = 270;
@@ -16,12 +14,10 @@ var TEXL_LINE = 20;
 var MAX_COLUMN_HEIGHT = 150;
 var COLUMN_WIDTH = 40;
 var COLUMN_GAP = 50;
-var COLUMN_GAP_LEFT = 100;
+var COLUMN_GAP_LEFT = 20;
 var COLUNM_GAP_TOP = 20;
-var columnColorYou = 'rgba(255, 0, 0, 1)'
+var COLUMN_COLOR_YOU = 'rgba(255, 0, 0, 1)'
 
-
-// Функции и переменные
 var drawRect = function(ctx, x, y, width, height, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
@@ -49,30 +45,27 @@ var randomNumberFromInterval = function(min, max) {
     return randomNumber;
 };
 
+var COLUMN_COLOR_PLAYER = "hsl(240, 100%," + randomNumberFromInterval(1, 100) + "%)"
+
 window.renderStatistics = function(ctx, names, times) {
-// Рисует облако
+
   drawRect(ctx, CLOUD_X + SHADOW_SHIFT, CLOUD_Y + SHADOW_SHIFT, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_SHADOW);
   drawRect(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_COLOR);
   drawText(ctx, TEXT_CONG, CLOUD_X + TEXT_GAP, CLOUD_Y + TEXT_GAP, TEXT_STYLE, TEXT_COLOR);
   drawText(ctx, TEXT_RESULT, CLOUD_X + TEXT_GAP, CLOUD_Y + TEXT_GAP + TEXL_LINE, TEXT_STYLE, TEXT_COLOR);
 
-   for (var i = 0; i < names.lenght; i++) {
-    // Я не поняла, почему у тебя в примере
-    //"рисовать столбцы
-    // for (var i = 0; i < COL_SIZE; i++)''
-    // https://codepen.io/sashasushko/pen/MVzQpp?editors=0010
 
-// рисует столбики
+   for (var i = 0; i < names.length; i++) {
+    if (names[i] === 'Вы') {
+      COLUMN_COLOR_PLAYER = COLUMN_COLOR_YOU;
+    } else {
+      COLUMN_COLOR_PLAYER = "hsl(240, 100%," + randomNumberFromInterval(2, 100) + "%)";
+  }
+
       var columnHeight = (MAX_COLUMN_HEIGHT * times[i]) / getMaxTime(times);
 
-      drawRect(ctx, CLOUD_X + COLUMN_GAP_LEFT + (COLUMN_WIDTH + COLUMN_GAP) * i, CLOUD_Y + COLUNM_GAP_TOP + (columnHeight - MAX_COLUMN_HEIGHT) + columnHeight, COLUMN_WIDTH, columnHeight, columnColorPlayer);
-      drawText(ctx, names[i], CLOUD_X + COLUMN_GAP_LEFT + (COLUMN_WIDTH + COLUMN_GAP) * i, CLOUD_Y + COLUNM_GAP_TOP + (columnHeight - MAX_COLUMN_HEIGHT) + columnHeight + TEXL_LINE, TEXT_STYLE, TEXT_COLOR);
-      drawText(ctx, Math.floor(times[i]), CLOUD_X + COLUMN_GAP_LEFT + (COLUMN_WIDTH + COLUMN_GAP) * i, CLOUD_Y + COLUNM_GAP_TOP + (columnHeight - MAX_COLUMN_HEIGHT) - TEXL_LINE, TEXT_STYLE, TEXT_COLOR);
-
-      if (names[i] === 'Вы') {
-        columnColorPlayer = columnColorYou;
-      } else {
-        columnColorPlayer = "hsl(240, 100%," + randomNumberFromInterval(1, 100) + "%)";
-      }
-   }
+      drawRect(ctx, CLOUD_X + COLUMN_GAP_LEFT + (COLUMN_WIDTH + COLUMN_GAP) * i, CLOUD_HEIGHT - columnHeight - TEXL_LINE, COLUMN_WIDTH, columnHeight, COLUMN_COLOR_PLAYER);
+      drawText(ctx, names[i], CLOUD_X + COLUMN_GAP_LEFT + (COLUMN_WIDTH + COLUMN_GAP) * i, CLOUD_HEIGHT, TEXT_STYLE, TEXT_COLOR);
+      drawText(ctx, Math.floor(times[i]), CLOUD_X + COLUMN_GAP_LEFT + (COLUMN_WIDTH + COLUMN_GAP) * i, CLOUD_HEIGHT - columnHeight - TEXL_LINE*2, TEXT_STYLE, TEXT_COLOR);
+  }
 };
